e03a01afb70ec86aebeadb2cc60b0525332afa2c
mohsen-ziadi <mohzia1380@gmail.com>
2025-06-12 21:05:02 +0330

    initial program

---
+++ code/config.js
@@ -0,0 +1,10 @@
+const cryptoHash = require("./crypto-hash")
+
+const GENESIS_DATA = {
+    timestamp: 14040322,
+    lastHash: "Hash not found",
+    data: [],
+    hash: cryptoHash(14040322, "lastHash", [])
+}
+
+module.exports = { GENESIS_DATA }
