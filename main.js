52f25577aa6f6ba652e5c801b317928b2c64809d
mohsen-ziadi <mohzia1380@gmail.com>
2025-06-26 20:31:00 +0330

    move files

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const Blockchain = require('./blockchaiApp/code/blockchain')
+const Blockchain = require('./blockchainApp/code/blockchain')
 const api = require('./app/api');
 
 const app = express();
@@ -8,3 +8,11 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', api);
+
+app.use(function(req, res, next) {
+	next({ status: 404, success: false, message: '404, Not Found!' });
+});
+
+app.use(handler);
